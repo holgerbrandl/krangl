78a30a26d46ec6ce4aa1d18d87653e4b3d96dd7f
Holger Brandl <brandl@mpi-cbg.de>
2018-04-11 09:31:15 +0200

    added irisData javadoc

--- src/main/kotlin/krangl/TableIO.kt
+++ src/main/kotlin/krangl/TableIO.kt
@@ -333,6 +333,25 @@ val sleepPatterns by lazy {
 }
 
 
+/**
+ * This famous (Fisher's or Anderson's) iris data set gives the measurements in centimeters
+ * of the variables sepal length and width and petal length and width, respectively, for 50
+ * flowers from each of 3 species of iris. The species are Iris setosa, versicolor, and virginica.
+ *
+ * ## Format
+ *
+ * iris is a data frame with 150 cases (rows) and 5 variables (columns) named `Sepal.Length`, `Sepal.Width`, `Petal.Length`, `Petal.Width`, and `Species`.
+ *
+ * ## Source
+ * Fisher, R. A. (1936) The use of multiple measurements in taxonomic problems. Annals of Eugenics, 7, Part II, 179–188.
+ *
+ * The data were collected by Anderson, Edgar (1935). The irises of the Gaspe Peninsula, Bulletin of the American Iris Society, 59, 2–5.
+ *
+ * ## References
+ * Becker, R. A., Chambers, J. M. and Wilks, A. R. (1988) The New S Language. Wadsworth & Brooks/Cole. (has iris3 as iris.)
+ *
+ *
+ */
 val irisData = DataFrame.readDelim(DataFrame::class.java.getResourceAsStream("data/iris.txt"), format = CSVFormat.TDF.withHeader())
 
 
