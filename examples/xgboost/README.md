10d33ca619dd45887d9e57602e9cd2c38a4864d9
Holger Brandl <holgerbrandl@users.noreply.github.com>
2018-09-28 08:33:58 +0200

    cont xgboost examples

---
+++ examples/xgboost/README.md
@@ -0,0 +1,24 @@
+# Krangl examples using XGBoost
+
+https://github.com/dmlc/xgboost which is a scalable and flexible Gradient Boosting library
+
+https://xgboost.ai/
+
+# General References
+
+https://github.com/dmlc/xgboost/tree/master/jvm-packages/xgboost4j-example
+
+https://medium.com/blablacar-tech/fast-machine-learning-predictions-6856b3623e5
+
+https://github.com/jtablesaw/tablesaw/issues/151
+
+https://github.com/dmlc/xgboost/blob/master/jvm-packages/xgboost4j-example/src/main/java/ml/dmlc/xgboost4j/java/example/BasicWalkThrough.java
+
+
+## For regression
+
+https://www.kaggle.com/fashionlee/using-xgboost-for-regression
+
+https://www.kaggle.com/mburakergenc/predictions-with-xgboost-and-linear-regression
+
+https://xgboost.readthedocs.io/en/latest/parameter.html#learning-task-parameters
---
+++ examples/xgboost/src/main/kotlin/OriginalDemo.kts
@@ -0,0 +1,66 @@
+@file:MavenRepository("repo1", "https://jitpack.io")
+
+
+@file:DependsOn("com.github.holgerbrandl:kscript-annotations:1.2")
+@file:DependsOn("de.mpicbg.scicomp:krangl:0.11-SNAPSHOT")
+@file:DependsOn("com.github.holgerbrandl:kravis:0.4-SNAPSHOT")
+@file:DependsOn("ml.dmlc:xgboost4j:0.80")
+
+import ml.dmlc.xgboost4j.java.DMatrix
+import ml.dmlc.xgboost4j.java.XGBoost
+
+
+// build xgboost model
+//wget --no-check-certificate https://raw.githubusercontent.com/dmlc/xgboost/master/demo/data/agaricus.txt.test
+//wget --no-check-certificate https://raw.githubusercontent.com/dmlc/xgboost/master/demo/data/agaricus.txt.train
+//wget --no-check-certificate https://raw.githubusercontent.com/dmlc/xgboost/master/demo/data/featmap.txt
+
+val trainMat = DMatrix("agaricus.txt.test")
+val testMat = DMatrix("agaricus.txt.train")
+//val validMat = DMatrix("valid.svm.txt") // todo where is this one?
+
+val params = hashMapOf<String, Any>().apply {
+    put("eta", 1.0)
+    put("max_depth", 2)
+    put("silent", 1)
+    put("objective", "binary:logistic")
+    put("eval_metric", "logloss")
+}
+
+// https://www.kaggle.com/fashionlee/using-xgboost-for-regression
+//our_params={'eta':0.1,'seed':0,'subsample':0.8,'colsample_bytree':0.8,'objective':'reg:linear','max_depth':3,'min_child_weight':1}
+
+// explore the training data
+testMat.rowNum()
+testMat.rowNum()
+
+
+// Specify a watch list to see model accuracy on data sets
+val watches = hashMapOf<String, DMatrix>().apply {
+    put("train", trainMat)
+    put("test", testMat)
+}
+
+val nround = 2
+val booster = XGBoost.train(trainMat, params, nround, watches, null, null)
+
+// build feature map
+// https://stackoverflow.com/questions/37627923/how-to-get-feature-importance-in-xgboost
+// dump with feature map
+//        booster.getFeatureScore()
+//        booster.imp
+val model_dump_with_feature_map = booster.getModelDump("featmap.txt", false)
+
+//        predict on test set
+//var dtest = DMatrix("test.svm.txt")
+// predict
+var predicts = booster.predict(testMat)
+
+predicts.joinToString(",")
+predicts.first().joinToString(",")
+
+// calculate confusion matrix
+predicts
+
+
+//booster.getFeatureScore()
