5c3f985163b215350ed9eb9f7ecc74d731db765c
Holger Brandl <holgerbrandl@users.noreply.github.com>
2020-07-02 23:19:02 +0200

    Further improved build automation

--- docs/krangl_release.sh
+++ docs/krangl_release.sh
@@ -1,9 +1,20 @@
-## Release Checklist
+# krangl release automation
 
+# adjust to te path of your working copy
 export KRANGL_HOME=/mnt/hgfs/sharedDB/db_projects/krangl/
 
+#########################################################################
+## Update release notes in [CHANGES.md](../CHANGES.md)
+
+
+
+#########################################################################
+## Run tests locally
+
+./gradlew check
+
 ########################################################################
-#1. Increment version in readme, gradle, example-poms and
+## Increment version in readme, gradle, example-poms and
 
 #**{todo}** automate this
 
@@ -13,10 +24,16 @@ krangl_version=$(grep '^version' ${KRANGL_HOME}/build.gradle | cut -f2 -d' ' | t
 
 echo "new version is $krangl_version"
 
-########################################################################
-#2. Rebuild the javadoc
 
-gradle clean dokka
+#change to use java8 for building because of https://github.com/Kotlin/dokka/issues/294
+# update-java-alternatives -l
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+
+
+########################################################################
+## Rebuild the javadoc
+
+./gradlew clean dokka
 
 git rm -r --cached ${KRANGL_HOME}/docs/javadoc
 
@@ -24,15 +41,24 @@ rm -rf ${KRANGL_HOME}/docs/javadoc
 cp -r ${KRANGL_HOME}/javadoc ${KRANGL_HOME}/docs/javadoc
 git add -A ${KRANGL_HOME}/docs/javadoc
 
-git commit -m "updated java for v${krangl_version} release"
+git commit -m "updated javadoc for v${krangl_version} release"
 
+#########################################################################
+## Push and wait for travis CI results
 
+# Note: From here on the steps can only be executed by the repo maintainer(s)
 
-#3. Update [CHANGES.md](../CHANGES.md)
+# make sure that are no pending chanes
+#(git diff --exit-code && git tag v${kscript_version})  || echo "could not tag current branch"
+git diff --exit-code  || echo "There are uncomitted changes"
 
-#4. Push and wait for travis CI results
+git push
 
+#########################################################################
 #5. Do the release
+
+# dokka related gradle deprecation warnings https://github.com/Kotlin/dokka/issues/515
+./gradlew assemble
 
 
 
