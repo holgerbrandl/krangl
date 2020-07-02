50d4dff9bbe954b22e1b55d5b70ab8699a5de570
Holger Brandl <holgerbrandl@users.noreply.github.com>
2020-07-02 21:26:30 +0200

    Improved release automation

--- docs/krangl_release.sh
+++ docs/krangl_release.sh
@@ -1,29 +1,40 @@
 ## Release Checklist
 
-1. Increment version in readme, gradle, example-poms and
+export KRANGL_HOME=/mnt/hgfs/sharedDB/db_projects/krangl/
 
-2. Rebuild the javadoc
+########################################################################
+#1. Increment version in readme, gradle, example-poms and
 
-```bash
-gradle clean dokka
-```
+#**{todo}** automate this
 
-3. Update [CHANGES.md](../CHANGES.md)
-
-4. Push and wait for travis CI results
-
-5. Do the release
-
-```bash
-
-
-#export KRANGL_HOME="/Users/brandl/projects/kotlin/krangl";
-export KRANGL_HOME="/d/projects/misc/krangl";
 
 trim() { while read -r line; do echo "$line"; done; }
 krangl_version=$(grep '^version' ${KRANGL_HOME}/build.gradle | cut -f2 -d' ' | tr -d "'" | trim)
 
 echo "new version is $krangl_version"
+
+########################################################################
+#2. Rebuild the javadoc
+
+gradle clean dokka
+
+git rm -r --cached ${KRANGL_HOME}/docs/javadoc
+
+rm -rf ${KRANGL_HOME}/docs/javadoc
+cp -r ${KRANGL_HOME}/javadoc ${KRANGL_HOME}/docs/javadoc
+git add -A ${KRANGL_HOME}/docs/javadoc
+
+git commit -m "updated java for v${krangl_version} release"
+
+
+
+#3. Update [CHANGES.md](../CHANGES.md)
+
+#4. Push and wait for travis CI results
+
+#5. Do the release
+
+
 
 ### Do the github release
 ## see https://github.com/aktau/github-release
@@ -67,14 +78,12 @@ gradle install
 
 # careful with this one!
 gradle bintrayUpload
-```
 
-For released versions check:
+#For released versions check:
+#
+#- https://bintray.com/holgerbrandl/mpicbg-scicomp/krangl
+#- https://jcenter.bintray.com/de/mpicbg/scicomp/krangl/
+#
 
-- https://bintray.com/holgerbrandl/mpicbg-scicomp/krangl
-- https://jcenter.bintray.com/de/mpicbg/scicomp/krangl/
-
---
-
-4. Increment version to *-SNAPSHOT for next release cycle
+#4. Increment version to *-SNAPSHOT for next release cycle
 
