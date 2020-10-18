6f1f0f0cb74056853d4550b7160b0c133c8244c4
LeandroC89 <leandroadao3@gmail.com>
2020-10-18 19:01:49 +0100

    - Refactored Excel tests
    Signed-off-by: LeandroC89 <leandroadao3@gmail.com>

--- src/test/kotlin/krangl/test/BuilderTests.kt
+++ src/test/kotlin/krangl/test/BuilderTests.kt
@@ -273,57 +273,99 @@ class JsonTests {
 class ExcelTests {
 
     @Test
-    fun `it should read excel file`(){
-        val headerHigherThanContentDF = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
-                "FirstSheet",CellRangeAddress.valueOf("A105:A110" ))
+    fun `readExcel -  should read excel file`(){
 
-        headerHigherThanContentDF shouldBe emptyDataFrame()
-
-        //Test Sheet by name starting at row 1
         val df = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
-                "FirstSheet", colTypes = mapOf(Pair("Activities", ColType.Int)))
+                "FirstSheet")
 
         df.names shouldBe listOf("Name", "Email", "BirthDate", "Country", "Activities", "Registered")
         df.nrow shouldBe 100
         df["Name"][0] shouldBe "Hyatt"
-        (df["Activities"] is IntCol) shouldBe true // Tests when ColType is given
-        (df["Registered"] is BooleanCol) shouldBe true // Tests when ColType is guessed
         df.print(title = "ExcelReadTest")
         println(df.schema())
+    }
+
+    @Test
+    fun `readExcel - sheet by name should match sheet by index`(){
+        val nameDF = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
+                "FirstSheet")
+
+        val indexDF = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
+                0)
+
+        nameDF shouldBe indexDF
+    }
+
+    @Test
+    fun `readExcel - out of range test`() {
+        val headerHigherThanContentDF = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
+                "FirstSheet", CellRangeAddress.valueOf("A105:A110"))
+
+        headerHigherThanContentDF shouldBe emptyDataFrame()
+    }
+
+    @Test
+    fun `readExcel - range test`(){
+        val df = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
+                "FirstSheet")
 
         // Test sheet by index + cell range
         val cellRangeTestDF = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
-                sheet = 1, trim_ws = true, cellRange = CellRangeAddress.valueOf("E3:J103" ) )
-
-        cellRangeTestDF shouldBe df
+                sheet = 1, cellRange = CellRangeAddress.valueOf("E3:J103" ), trim_ws = true )
 
         // Test defaulted cellRange's correctness on sheet with empty rows/cols
         val defaultCellRangeTestDF = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
-                sheet = 1, trim_ws = true )
+                sheet = 1, trim_ws = true)
 
+        cellRangeTestDF shouldBe df
         defaultCellRangeTestDF shouldBe cellRangeTestDF
+    }
 
-        // Test stop at blank lines
+    @Test
+    fun `readExcel - trim_ws should trim white space`(){
+        val df = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
+                "FirstSheet")
+        val trimmedDF = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
+                sheet = 1, trim_ws = true)
+
+        trimmedDF shouldBe df
+    }
+
+    @Test
+    fun `readExcel - colTypes should work`(){
+        val df = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
+                "FirstSheet", colTypes = mapOf(Pair("Activities", ColType.Int)))
+
+        (df["Activities"] is IntCol) shouldBe true // Tests when ColType is given
+        (df["Registered"] is BooleanCol) shouldBe true // Tests when ColType is guessed
+    }
+
+    @Test
+    fun `readExcel - should stop at first blank line`(){
         val shouldStopAtBlankDF = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
                 sheet = 2, trim_ws = true, cellRange = CellRangeAddress.valueOf("E3:J10" ))
 
         shouldStopAtBlankDF.nrow shouldBe 4
+    }
 
-        // Test continue past blank lines
-        var shouldContinueAtBlankDF = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
+    @Test
+    fun `readExcel - should continue past blank line`(){
+        val shouldContinueAtBlankDF = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
                 sheet = 2, trim_ws = true, cellRange = CellRangeAddress.valueOf("E3:J10" ), stopAtBlankLine = false )
 
         shouldContinueAtBlankDF.nrow shouldBe 6
+    }
 
-        // Test continue past blank lines
-        shouldContinueAtBlankDF = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
+    @Test
+    fun `readExcel - should include blank lines`(){
+        val shouldContinueAtBlankDF = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
                 sheet = 2, trim_ws = true, cellRange = CellRangeAddress.valueOf("E3:J10" ), stopAtBlankLine = false, includeBlankLines = true )
 
         shouldContinueAtBlankDF.nrow shouldBe 7
-
     }
+
     @Test
-    fun `it should write to excel`(){
+    fun `writeExcel - should write to excel`(){
         val df = DataFrame.readExcel("src/test/resources/krangl/data/ExcelReadExample.xlsx",
                 "FirstSheet")
 
