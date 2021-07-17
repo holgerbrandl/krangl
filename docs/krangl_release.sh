53d8a1a05c5ea9b5d5a9f6bf82248274b96e3933
Holger Brandl <holgerbrandl@users.noreply.github.com>
2021-07-17 10:46:25 +0200

    Simplified build process

--- docs/krangl_release.sh
+++ docs/krangl_release.sh
@@ -83,8 +83,7 @@ git push origin --tags
 # careful with this one!
 # https://getstream.io/blog/publishing-libraries-to-mavencentral-2021/
 # https://central.sonatype.org/pages/gradle.html
-./gradlew publishReleasePublicationToSonatypeRepository
-./gradlew closeAndReleaseRepository
+ ./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository
 
 ## also see https://oss.sonatype.org/
 
